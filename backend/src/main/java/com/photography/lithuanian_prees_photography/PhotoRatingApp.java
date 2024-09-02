3ae72f1f76bbc158d88842476040c1401b7f2d32
Anton Isakov <anton.isakov@stud.techin.lt>
2024-09-02 17:16:26 +0300

    backend project created

---
+++ backend/src/main/java/com/photography/lithuanian_prees_photography/PhotoRatingApp.java
@@ -0,0 +1,13 @@
+package com.photography.lithuanian_prees_photography;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotoRatingApp {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotoRatingApp.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/photography/lithuanian_prees_photography/PhotoRatingAppTests.java
@@ -0,0 +1,13 @@
+package com.photography.lithuanian_prees_photography;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotoRatingAppTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
